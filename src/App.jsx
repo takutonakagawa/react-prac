406a01aa65251212cf2bf72701196f60fd06f701
tackt21 <82143268+takutonakagawa@users.noreply.github.com>
2021-09-05 19:00:31 +0900

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
-  const onClickBUtton = () => alert("aaa");
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(true);
 
-  contentStyle2 = {
-    color: "pink",
-    fontSize: "18px"
+  const onClickCountUp = () => {
+    setNum(num + 1);
   };
+
+  const onClickCountDown = () => {
+    if (num > 0) {
+      setNum(num - 1);
+    }
+  };
+
+  const onClickSwichShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  // useEfecctは第二引数を監視しその引数に変化があった場合に実行される関数
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+  }, [num]);
 
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは</h1>
       <ColorfulMessage color="blue">お元気ですか</ColorfulMessage>
       <ColorfulMessage color="pink">お元気</ColorfulMessage>
-
-      <button onClick={onClickBUtton}>ボタン</button>
+      <button onClick={onClickCountUp}>UP</button>
+      <br />
+      <button onClick={onClickCountDown}>DOWN</button>
+      <br />
+      <br />
+      <button onClick={onClickSwichShowFlag}>on/off</button>
+      <p>{num}</p>
+      {faceShowFlag && <p>(-_-)</p>}
     </>
   );
 };
