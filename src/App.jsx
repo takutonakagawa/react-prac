67f2b21f54c9351016f235a0feb922b44b29e116
tackt21 <82143268+takutonakagawa@users.noreply.github.com>
2021-09-05 15:33:28 +0900

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import React from "reavt";
+import React from "react";
 
 const App = () => {
+  const onClickBUtton = () => alert("aaa");
+  contentStyle = {
+    color: "blue",
+    fontSize: "18px"
+  };
+
   return (
     <>
-      <h1>こんにちは</h1>
-      <p>reactpractice</p>
+      <h1 style={{ color: "red" }}>こんにちは</h1>
+      <p style={contentStyle}>reactp1ractice</p>
+      <button onClick={onClickBUtton}>ボタン</button>
     </>
   );
 };
